a92f2b77725d3009fb13fa6aeecec310d7370eb7
kokia <opportunity.curiosity@gmail.com>
2019-06-29 03:06:56 +0900

    wave のtype追加準備

--- wave.h
+++ wave.h
@@ -1,20 +1,24 @@
 //wave generator
 #ifndef ___WAVE_H
 #define ___WAVW_H
-
 #include "config.h"
 #include "utilize.h"
+
+//type
+#define CONST 0
+#define SIN 1
+#define TRIANGLE 2
 
 namespace kal{
 
 class wave{
   public:
     int t;
+    int type;//@todo:色々な波形を追加
     double ave;
     double amp;
     double f;
     double output;
-    char type;//いずれ色々な波形追加
     wave();
     wave(double ave_in,double amp_in,double f_in);
     void update();
@@ -26,6 +30,7 @@ wave::wave(){
   amp=0.0;
   f=0.0;
   output=0.0;
+  type = SIN;
 }
 
 wave::wave(double ave_in,double amp_in,double f_in){
@@ -33,12 +38,16 @@ wave::wave(double ave_in,double amp_in,double f_in){
   amp=amp_in;
   f=f_in;
   output=0.0;
+  type = SIN;
 }
 
 void wave::update(){
   t+=Ts;
-  t = t%(int)(1.0/f*1000);//tの∞積分を防止
-  output=amp*sin(2*PI*f*t/1000.0)+ave;
+  t = t%(int)(1.0/f);//tの∞積分を防止
+  if(type == SIN){
+    output=amp*sin(2.0*PI*f*t)+ave;
+  }
+  
 }
 
 }
