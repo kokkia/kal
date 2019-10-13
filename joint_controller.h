432a50525ab384721bba0724df69b631423342a5
kokia <opportunity.curiosity@gmail.com>
2019-10-13 16:15:53 +0900

    two wheel drive, joint controllerの改良，まだ実装は不可．
    Signed-off-by: kokia <opportunity.curiosity@gmail.com>

--- joint_controller.h
+++ joint_controller.h
@@ -12,7 +12,7 @@ class joint_controller{
         double Kp;
         double Ki;
         double Kd;
-        double Kdd;
+        double Kdd = 0.0;//基本使わない
         //ff_param
         double J;
         double D;
@@ -20,8 +20,11 @@ class joint_controller{
         //@todo:friction model 入れたい
         
         //関節角度
-        RobotData ref_bfr;//qのみ使用
-        RobotData state_bfr;
+        RobotData<double> ref;//qのみ使用
+        RobotData<double> state;
+        RobotData<double> ref_bfr;//qのみ使用
+        RobotData<double> state_bfr;
+        double integral = 0.0;
 
         //出力
         double output;
@@ -29,10 +32,10 @@ class joint_controller{
         void set_fb_param(double Kp,double Ki,double Kd);
         void set_ff_param(double J,double D,double K);
 
-        double feedforward_control(RobotData ref,RobotData state);
+        double feedforward_control(void);
         //double feedback_control(RobotData ref,RobotData state);
-        double position_control(RobotData ref,RobotData state);
-        double velocity_control(RobotData ref,RobotData state);
+        double position_control(void);
+        double velocity_control(void);
         //double two_DOF_control(RobotData ref,RobotData state);
 
 };
@@ -49,17 +52,24 @@ void joint_controller::set_ff_param(double J,double D,double K){
     this->K = K;
 }
 
-double joint_controller::feedforward_control(RobotData ref,RobotData state){
+double joint_controller::feedforward_control(){
+    output = J * ref.d2q + D * ref.dq + K * ref.q;
+    return output;
 }
 
 //double joint_controller::feedback_control(RobotData ref,RobotData state){
 //}
 
-double joint_controller::position_control(RobotData ref,RobotData state){
-    output = Kp * (ref.q - state.q) + Kd * (ref.dq - state.dq);
+double joint_controller::position_control(){
+    output = Kp * (ref.q - state.q) + Kd * (ref.dq - state.dq) + Ki * integral;
+    integral += (ref.q - state.q) * Ts;
+    return output;
 }
 
-double joint_controller::velocity_control(RobotData ref,RobotData state){
+double joint_controller::velocity_control(){
+    output = Kp * (ref.q - state.q) + Kd * (ref.dq - state.dq) + Kdd * (ref.d2q - state.d2q);
+    //@todo:ここでintegralした方がよいのか検討
+    return output;
 }
 
 
--- two_wheel_drive.h
+++ two_wheel_drive.h
@@ -1,10 +1,38 @@
 //two_wheel_drive.h
+//未完成
 #ifndef ___TWO_WHEEL_DRIVE_H
 #define ___TWO_WHEEL_DRIVE_H
 #include "config.h"
 #include "utilize.h"
+#include "joint_controller.h"
 
 namespace kal{
-    
+
+class two_wheel_drive{
+public:
+    RobotData<double> ref;
+    RobotData<double> state;
+    joint_controller motor[2];
+
+    double R;//wheel半径
+    double b;//タイヤ間距離
+
+    void set_param(double R, double b);
+    //void odmetry_update();
+    //void line_trace();
+    void velocity_control(double left_speed,double right_speed);
+};
+
+void two_wheel_drive::set_param(double R, double b){
+    this->R = R;
+    this->b = b;
+}
+
+void two_wheel_drive::velocity_control(double left_speed,double right_speed){
+    motor[0].ref.dq = left_speed;
+    motor[1].ref.dq = right_speed;
+    motor[0].velocity_control();
+}
+
 }
 #endif
