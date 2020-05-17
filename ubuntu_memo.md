6ddd444b386b202e542199ff20f87e15e94216bd
kokkia <opportunity.curiosity@gmail.com>
2020-05-17 19:55:25 +0900

    無線LANにつなげるようにした

--- ubuntu_memo.md
+++ ubuntu_memo.md
@@ -1,4 +1,4 @@
-# Ubuntuトラブルメモ
+# Ubuntuメモ
 
 ## イヤホンから音が出ないときの対処法  
 参考(https://forums.ubuntulinux.jp/viewtopic.php?pid=63098)
@@ -8,7 +8,13 @@ options snd-hda-intel model=basic
 と書き込み、保存、再起動で解決
 
 ## gnuplotのインストール
-
+$ sudo apt-get install gnuplot
+$ sudo apt-get intall gnuplot-x11
 
 
 ## nccatの使い方
+1. 送信側  
+nc -u remoteserver port  
+  
+1. 受信側  
+nc -u -l port
--- udp_for_esp32.h
+++ udp_for_esp32.h
@@ -1,4 +1,5 @@
 //esp32用udp通信用ヘッダ
+// esp32とPCでUDP通信すると高速なのは良いが、PCがインターネットにつなげなくなるのが現状問題
 #ifndef ___KAL_UDP_FOR_ESP32_H
 #define ___KAL_UDP_FOR_ESP32_H
 #include <lwip/sockets.h>
@@ -7,29 +8,37 @@
 #include <WiFiUdp.h>
 #include "config.h"
 
+#define NO_NETWORK 0
+#define ISOLATED_NETWORK 1
+#define CONNECT_NETWORK 2
+
 #define PACKET_SIZE 256
-const char ssid[] = "ESP32_wifi"; // SSID
-const char pass[] = "esp32pass";  // password
-const int localPort = 10000;      // ポート番号
+
+//適当によく使う設定
+const char esp_ssid[] = "ESP32_wifi"; // SSID
+const char esp_pass[] = "esp32pass";  // password
 const IPAddress ip(111, 11, 1, 11);       // IPアドレス(ゲートウェイも兼ねる)
 const IPAddress subnet(255, 255, 255, 0); // サブネットマスク
 const IPAddress remoteip(111,11,1,12);
-const int remotePort = 10001;
+const int localPort = 4000;      // ポート番号
+const int remotePort = 4001;
 
 namespace kal {
 
 template<class T>
 class udp_for_esp32{
     public:
-
-    //UDP通信関連の定義
-    char packetbuffer[PACKET_SIZE]; 
+    int network;//独立ネットワークにするか無線LANにつなぐか
+    
+    char packetbuffer[PACKET_SIZE];
+    IPAddress rip = remoteip;
     WiFiUDP udp;
     // T* sbuffer;//送信用
     // T rbuffer;//受信用
 
-    udp_for_esp32();
-    void set_udp();
+    udp_for_esp32(int network);
+    void set_udp(const char* ssid,const char* pass);
+    void set_remoteip(IPAddress rip);
     char receive_char();
     T receive();
     void send_char(char c);
@@ -37,26 +46,48 @@ class udp_for_esp32{
 };
 
 template<class T>
-udp_for_esp32<T>::udp_for_esp32(){
+udp_for_esp32<T>::udp_for_esp32(int network){
+    this->network = network; 
 }
 
 template<class T>
-void udp_for_esp32<T>::set_udp(){
-    WiFi.softAP(ssid, pass);           // SSIDとパス設定
-    delay(100);                        // delay必要かも
-    WiFi.softAPConfig(ip, ip, subnet); // IPアドレス、ゲートウェイ、サブネットマスクの設定
-
-    Serial.print("AP IP address: ");
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.println(myIP);
-
+void udp_for_esp32<T>::set_udp(const char* ssid,const char* pass){
+    if(network == ISOLATED_NETWORK){//独立したネットワークを作成
+        WiFi.softAP(ssid, pass);           // SSIDとパス設定
+        delay(100);                        // delay必要かも
+        WiFi.softAPConfig(ip, ip, subnet); // IPアドレス、ゲートウェイ、サブネットマスクの設定
+        Serial.print("AP IP address: ");
+        IPAddress myIP = WiFi.softAPIP();
+        Serial.println(myIP);
+    }
+    else if(network == CONNECT_NETWORK){//  無線 LAN に接続
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(ssid, pass);             //  接続確立まで待つ
+        Serial.println("Connecting...");
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(1000);
+        }
+        Serial.println("Connected");
+        Serial.println(WiFi.localIP());     //  ESP 自身の IP アドレスをログ出力
+        // //  mDNS の呼びかけに応答できるように仕込む
+        // Serial.println("Setting up mDNS...");
+        // if (MDNS.begin("esp32")){         //  "dongbei1.local" で応答
+        //     Serial.println("mDNS started");
+        // }else{
+        //     Serial.println("mDNS failed to start");
+        // }
+    }
     Serial.println("Starting UDP");
     udp.begin(localPort);  // UDP通信の開始(引数はポート番号)
-
     Serial.print("Local port: ");
     Serial.println(localPort);
 }
 
+template<class T>
+void udp_for_esp32<T>::set_remoteip(IPAddress rip){
+    this->rip = rip;
+    Serial.println(this->rip);
+}
 template<class T>
 char udp_for_esp32<T>::receive_char(){
     int packetsize = udp.parsePacket();
@@ -67,42 +98,44 @@ char udp_for_esp32<T>::receive_char(){
     return packetbuffer[0];
 }
 
+//char型以外の送受信面倒くさそう、一旦終了
 //コンパイルできたが、使えるか不明
-template<class T>
-T udp_for_esp32<T>::receive(){
-    T buffer;
-    int packetsize = udp.parsePacket();
-    if (packetsize>0) {
-        udp.read(&buffer,sizeof(buffer));
-        // Serial.print(packetbuffer); // UDP通信で来た値を表示
-    }
-    return buffer;
+// template<class T>
+// T udp_for_esp32<T>::receive(){
+//     T buffer;
+//     int packetsize = udp.parsePacket();
+//     if (packetsize>0) {
+//         udp.read(&buffer,sizeof(buffer));
+//         // Serial.print(packetbuffer); // UDP通信で来た値を表示
+//     }
+//     return buffer;
 
-}
+// }
 
 template<class T>
 void udp_for_esp32<T>::send_char(char c){
-    udp.beginPacket(remoteip,remotePort);
+    udp.beginPacket(rip,remotePort);
     udp.write(c);
     udp.endPacket();
 }
 
+//char型以外の送受信面倒くさそう、一旦終了
 //コンパイルできたが使えるか不明、自分でWiFiUDP.h書き換えた方が早いかも
-template<class T>
-void udp_for_esp32<T>::send(T sbuf){
-    udp.beginPacket(remoteip,remotePort);
-    // udp.write(&sbuf,sizeof(sbuf));
-    // udp.endPacket();
-    struct sockaddr_in recipient;
-    recipient.sin_addr.s_addr = (uint32_t)remoteip;
-    recipient.sin_family = AF_INET;
-    recipient.sin_port = htons(remotePort);
-    int sent = sendto(socket(AF_INET, SOCK_DGRAM, 0), &sbuf, sizeof(sbuf), 0, (struct sockaddr*) &recipient, sizeof(recipient));
-    if(sent < 0){
-        log_e("could not send data: %d", errno);
-        // return 0;
-    }
-}
+// template<class T>
+// void udp_for_esp32<T>::send(T sbuf){
+//     udp.beginPacket(remoteip,remotePort);
+//     // udp.write(&sbuf,sizeof(sbuf));
+//     // udp.endPacket();
+//     struct sockaddr_in recipient;
+//     recipient.sin_addr.s_addr = (uint32_t)remoteip;
+//     recipient.sin_family = AF_INET;
+//     recipient.sin_port = htons(remotePort);
+//     int sent = sendto(socket(AF_INET, SOCK_DGRAM, 0), &sbuf, sizeof(sbuf), 0, (struct sockaddr*) &recipient, sizeof(recipient));
+//     if(sent < 0){
+//         log_e("could not send data: %d", errno);
+//         // return 0;
+//     }
+// }
     
 } // namespace name
 
